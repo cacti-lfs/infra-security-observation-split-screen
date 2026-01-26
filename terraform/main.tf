7bd67f2bfe20f90ef1cd4277a1361b579f2db69a
cacti-lfs <lois.dutour@outlook.fr>
2026-01-26 11:43:35 +0100

    terraform:ajout output bastion

--- terraform/main.tf
+++ terraform/main.tf
@@ -231,16 +231,20 @@ resource "proxmox_vm_qemu" "vm_bastion" {
 output "proxmox_vms" {
   value = {
     "reverse_proxy" = {
-      ip   = proxmox_vm_qemu.vm_reverse_proxy.default_ipv4_address
-      name = proxmox_vm_qemu.vm_reverse_proxy.name
+        ip   = proxmox_vm_qemu.vm_reverse_proxy.default_ipv4_address
+        name = proxmox_vm_qemu.vm_reverse_proxy.name
     }
     "appli" = {
-      ip   = proxmox_vm_qemu.vm_appli.default_ipv4_address
-      name = proxmox_vm_qemu.vm_appli.name
+        ip   = proxmox_vm_qemu.vm_appli.default_ipv4_address
+        name = proxmox_vm_qemu.vm_appli.name
     }
     "supervision" = {
-      ip   = proxmox_vm_qemu.vm_supervision.default_ipv4_address
-      name = proxmox_vm_qemu.vm_supervision.name
+        ip   = proxmox_vm_qemu.vm_supervision.default_ipv4_address
+        name = proxmox_vm_qemu.vm_supervision.name
+    }
+    "bastion" = {
+        ip = proxmox_vm_qemu.vm_bastion.default_ipv4_address
+        name = proxmox_vm_qemu.vm_bastion.name
     }
   }
 }
