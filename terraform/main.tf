2d52f4de944e2ce571254c8204bb97026c078216
cacti-lfs <lois.dutour@outlook.fr>
2026-01-26 11:11:42 +0100

    terraform:output ajoutés

--- terraform/main.tf
+++ terraform/main.tf
@@ -173,3 +173,20 @@ resource "proxmox_vm_qemu" "vm_supervision" {
     # Configuration SSH
     sshkeys = var.ssh_rsa_key
 }
+# Création d'une map pour faciliter le parsing par Ansible
+output "proxmox_vms" {
+  value = {
+    "reverse_proxy" = {
+      ip   = proxmox_vm_qemu.vm_reverse_proxy.default_ipv4_address
+      name = proxmox_vm_qemu.vm_reverse_proxy.name
+    }
+    "appli" = {
+      ip   = proxmox_vm_qemu.vm_appli.default_ipv4_address
+      name = proxmox_vm_qemu.vm_appli.name
+    }
+    "supervision" = {
+      ip   = proxmox_vm_qemu.vm_supervision.default_ipv4_address
+      name = proxmox_vm_qemu.vm_supervision.name
+    }
+  }
+}
