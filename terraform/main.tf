391371b0f90cbad59d7be08282aec732f6ab5fef
cacti-lfs <lois.dutour@outlook.fr>
2026-01-26 10:12:00 +0100

    terraform:ajout vm sup+appli

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,116 @@ provider "proxmox" {
     pm_tls_insecure = true
 }
 
+# VM Reverse Proxy + Firewall
 resource "proxmox_vm_qemu" "vm_reverse_proxy" {
     # Nom de la VM
     name = "01-SRV-RVPRX-TRIX" # A personnaliser
+    vmid = 200
+    target_node = "proxmox"
+    
+    # Nom du template exact du template Proxmox
+    clone = "debian-13.3.0-cloud-template" # A personnaliser
+
+    # Type de clone (full#linked)
+    full_clone= true
+
+    # Configuration système (DOIT être cohérent avec template)
+    agent = 1
+    os_type = "cloud-init"
+
+    # Ressources (Possibilité de surcharger les valeurs du template ici)
+    cpu {
+        type = "host"
+        cores = 1
+        sockets = 1
+    }
+    memory = 2048
+
+    # Réseau
+    network {
+        model = "virtio"
+        bridge = "vmbr0"
+        id = 0
+    }
+    skip_ipv6 = true
+    # Disque
+    # Spécifier taille + stockage pour que Terraform sache où mettre le clone
+    disk {
+        slot = "scsi0"
+        size = "25"
+        type = "disk"
+        storage = "local-lvm"
+    }
+    vga {
+        type = "std"
+    }
+    # Ordre du boot
+    boot = "order=scsi0;net0"
+
+    # cloud-init
+    ipconfig0 = "ip=dhcp"
+
+    # Configuration SSH
+    sshkeys = var.ssh_rsa_key
+}
+
+# VM Applicatif
+resource "proxmox_vm_qemu" "vm_appli" {
+    # Nom de la VM
+    name = "01-SRV-APPLI-TRIX" # A personnaliser
+    vmid = 201
+    target_node = "proxmox"
+    
+    # Nom du template exact du template Proxmox
+    clone = "debian-13.3.0-cloud-template" # A personnaliser
+
+    # Type de clone (full#linked)
+    full_clone= true
+
+    # Configuration système (DOIT être cohérent avec template)
+    agent = 1
+    os_type = "cloud-init"
+
+    # Ressources (Possibilité de surcharger les valeurs du template ici)
+    cpu {
+        type = "host"
+        cores = 1
+        sockets = 1
+    }
+    memory = 2048
+
+    # Réseau
+    network {
+        model = "virtio"
+        bridge = "vmbr0"
+        id = 0
+    }
+    skip_ipv6 = true
+    # Disque
+    # Spécifier taille + stockage pour que Terraform sache où mettre le clone
+    disk {
+        slot = "scsi0"
+        size = "25"
+        type = "disk"
+        storage = "local-lvm"
+    }
+    vga {
+        type = "std"
+    }
+    # Ordre du boot
+    boot = "order=scsi0;net0"
+
+    # cloud-init
+    ipconfig0 = "ip=dhcp"
+
+    # Configuration SSH
+    sshkeys = var.ssh_rsa_key
+}
+
+# VM Supervision
+resource "proxmox_vm_qemu" "vm_supervision" {
+    # Nom de la VM
+    name = "01-SRV-SUP-TRIX" # A personnaliser
     vmid = 200
     target_node = "proxmox"
     
