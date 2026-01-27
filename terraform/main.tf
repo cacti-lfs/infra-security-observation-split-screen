06c5b8e328ee6f45147eb67b21c2e68eae318373
cacti-lfs <lois.dutour@outlook.fr>
2026-01-27 20:30:43 +0100

    terraform:fix cloud-init

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "proxmox_vm_qemu" "vm_reverse_proxy" {
     # Création du compte local
     ciuser = "cloudadm"
     cipassword = var.cloudadm_password
-    # cicustom = "user=local:snippets/user_data_trix.yml"
+
 
     # Ressources (Possibilité de surcharger les valeurs du template ici)
     cpu {
@@ -93,7 +93,7 @@ resource "proxmox_vm_qemu" "vm_appli" {
     # Création du compte local
     ciuser = "cloudadm"
     cipassword = var.cloudadm_password
-    # cicustom = "user=local:snippets/user_data_trix.yml"
+
 
     # Ressources (Possibilité de surcharger les valeurs du template ici)
     cpu {
@@ -151,7 +151,7 @@ resource "proxmox_vm_qemu" "vm_supervision" {
     # Création du compte local
     ciuser = "cloudadm"
     cipassword = var.cloudadm_password
-    # cicustom = "user=local:snippets/user_data_trix.yml"
+
 
     # Ressources (Possibilité de surcharger les valeurs du template ici)
     cpu {
@@ -209,7 +209,7 @@ resource "proxmox_vm_qemu" "vm_bastion" {
     # Création du compte local
     ciuser = "cloudadm"
     cipassword = var.cloudadm_password
-    # cicustom = "user=local:snippets/user_data_trix.yml"
+
 
     # Ressources (Possibilité de surcharger les valeurs du template ici)
     cpu {
